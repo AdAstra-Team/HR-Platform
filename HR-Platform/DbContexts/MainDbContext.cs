7776fb1e5190d3fa26a02f6c4a460b3eef6da5ec
Alexander Afonin <63101279+lueFlake@users.noreply.github.com>
2023-05-23 20:50:47 +0300

    db in docker >:D

---
+++ HR-Platform/DbContexts/MainDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class MainDbContext : DbContext
+{
+    public MainDbContext(DbContextOptions<MainDbContext> options) : base(options) { }
+
+    // Define your entity sets as DbSet properties here
+    // Example: public DbSet<User> Users { get; set; }
+}
--- HR-Platform/Program.cs
+++ HR-Platform/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MainDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("MainDbConnection")));
 
 var app = builder.Build();
 
