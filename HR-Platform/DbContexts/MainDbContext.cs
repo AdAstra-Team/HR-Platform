e1bda1d344f4731c66e7d0c736fed54f150193c6
Alexander Afonin <63101279+lueFlake@users.noreply.github.com>
2023-05-25 14:10:59 +0300

    FINALLY I FIXED NUGET!!!!!!!!!!!

--- HR-Platform/DbContexts/MainDbContext.cs
+++ HR-Platform/DbContexts/MainDbContext.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 public class MainDbContext : DbContext
 {
     public MainDbContext(DbContextOptions<MainDbContext> options) : base(options) {
+        Database.EnsureDeleted();
         Database.EnsureCreated();
     }
 
